1422dd972878d5d77d169718474a216ea4546a25
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2022-10-10 19:10:31 +0200

    Add files via upload

---
+++ ComboBoxKontrola/ComboBoxKontrola/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ComboBoxKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Algoritmi i programiranje",
+            "Napredno i objektno programiranje",
+            "Fizika",
+            "Računalne mreže",
+            "Matematika"});
+            this.comboBox1.Location = new System.Drawing.Point(44, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Odaberi jedan od ponuđenih predmeta:";
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(45, 114);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(217, 23);
+            this.txtBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 199);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private TextBox txtBox;
+    }
+}
---
+++ ComboBoxKontrola/ComboBoxKontrola/Form1.cs
@@ -0,0 +1,35 @@
+﻿namespace ComboBoxKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                txtBox.Text = "Algoritmi i programiranje";
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                txtBox.Text = "Napredno i objektno programiranje";
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                txtBox.Text = "Fizika";
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                txtBox.Text = "Računalne mreže";
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                txtBox.Text = "Matematika";
+            }
+
+        }
+    }
+}
